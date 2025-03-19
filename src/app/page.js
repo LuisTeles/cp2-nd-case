b3131ae0f86fa24d8b9ee4fe2ca652fa35bcc907
LUÍS ANTONIO RIBEIRO TELES <luis.teles@gec.inatel.br>
2025-03-18 22:51:44 -0300

    finished???

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
-"use client";
+"use client"
 
+import { useState } from "react";
 import Image from "next/image";
 import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
-import { useState } from "react";
 import TransactionItem from "@/components/TransactionItem";
 import Modal from "@/components/Modal";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(""); // State to store the search term
 
   const [transactions, setTransactions] = useState([
     {
@@ -38,7 +39,6 @@ export default function Home() {
     {
       title: "Computador",
       price: 5400,
-      price: 7200,
       category: "Serviço",
       date: "05/04/2022",
       type: "entrada",
@@ -52,41 +52,34 @@ export default function Home() {
     },
   ]);
 
-
+  // Function to add new transactions
   const addTransaction = (newTransaction) => {
     setTransactions([...transactions, newTransaction]);
   };
 
+  // Function to calculate total "entradas" and "saidas"
   const totalEntradas = transactions
     .filter((transaction) => transaction.type === "entrada")
     .reduce((acc, transaction) => acc + transaction.price, 0);
 
   const totalSaidas = transactions
-    .filter((transaction) => transaction.type === "saida")      
-    .reduce((acc, transaction) => acc + transaction.price, 0);  
+    .filter((transaction) => transaction.type === "saida")
+    .reduce((acc, transaction) => acc + transaction.price, 0);
 
   const total = totalEntradas - totalSaidas;
 
-  // const addTransaction = () => {
-  //   setTransactions([
-  //     ...transactions,
-  //     {
-  //       title: "Desenvolvimento de site",
-  //       price: 12000,
-  //       category: "Venda",
-  //       date: "13/04/2022",
-  //     },
-  //   ]);
-  // }
+  // Filter transactions based on the search term
+  const filteredTransactions = transactions.filter((transaction) => 
+    transaction.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    transaction.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    transaction.date.includes(searchTerm)
+  );
 
   return (
     <div className="flex flex-col min-h-screen font-roboto text-white bg-[linear-gradient(to_bottom,_#121214_17%,_#202024_17%)] p-0 sm:px-10">
       {/* Header */}
       <header className="flex justify-between items-center p-4 w-full max-w-6xl mx-auto">
-        {/* Logo */}
         <Image src="/images/Logo.png" alt="logo" width={50} height={50} />
-
-        {/* Button */}
         <button
           onClick={() => setIsModalOpen(true)}
           className="bg-yellow-500 rounded-sm text-sm text-white font-bold sm:py-3 py-2 sm:px-4 px-2 transition-transform active:scale-95"
@@ -95,7 +88,7 @@ export default function Home() {
         </button>
       </header>
 
-      {/* Div Principal */}
+      {/* Main Content */}
       <main className="flex-1 p-4 items-center justify-center w-full max-w-6xl mx-auto">
         <div className="flex flex-col w-full max-w-5xl mx-auto">
           {/* Fluxo de caixa */}
@@ -104,8 +97,7 @@ export default function Home() {
               <div>
                 <div className="text-sm font-extralight pb-2">Entradas</div>
                 <div className="text-3xl font-semibold py-2 pr-2">
-                  {" "}
-                  R$ {totalEntradas.toFixed(2)}{" "}
+                  R$ {totalEntradas.toFixed(2)}
                 </div>
               </div>
               <ArrowUpCircleIcon className="h-7 w-7 text-green-300" />
@@ -114,8 +106,7 @@ export default function Home() {
               <div>
                 <div className="text-sm font-extralight pb-2">Saídas</div>
                 <div className="text-3xl font-semibold py-2 pr-2">
-                  {" "}
-                  R$ {totalSaidas.toFixed(2)}{" "}
+                  R$ {totalSaidas.toFixed(2)}
                 </div>
               </div>
               <ArrowDownCircleIcon className="h-7 w-7 text-red-400" />
@@ -124,46 +115,40 @@ export default function Home() {
               <div>
                 <div className="text-sm font-extralight pb-2">Total</div>
                 <div className="text-3xl font-semibold py-2 pr-2">
-                  {" "}
-                  R$ {total.toFixed(2)}{" "}
+                  R$ {total.toFixed(2)}
                 </div>
               </div>
               <CurrencyDollarIcon className="h-7 w-7 text-white" />
             </div>
           </div>
 
-          {/* Todas Transações */}
-          <div className="flex-1 flex-col justify-center w-full max-w-5xl mx-auto items-center lg:p-9">
-            {/* Busca */}
-            <div className="flex flex-row justify-between items-center gap-4 mb-4">
-              {/* Search Input */}
-              <input
-                type="text"
-                placeholder="Busque uma transação"
-                className="flex-1 bg-customblack text-white placeholder-gray-400 rounded-sm p-3 text-sm w-full sm:w-auto shadow-xl"
+          {/* Search Bar */}
+          <div className="flex flex-row justify-between items-center gap-4 mb-4">
+            <input
+              type="text"
+              placeholder="Busque uma transação"
+              className="flex-1 bg-customblack text-white placeholder-gray-400 rounded-sm p-3 text-sm w-full sm:w-auto shadow-xl"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)} // Update search term as user types
+            />
+            <button className="flex items-center ring-yellow-500 ring-2 rounded-sm p-3 text-sm text-yellow-500 font-bold transition-transform active:scale-95">
+              <MagnifyingGlassIcon className="h-5 w-5 text-yellow-500 mx-2" />
+              <div className="hidden sm:flex">Buscar</div>
+            </button>
+          </div>
+
+          {/* Transactions List */}
+          <div className="flex flex-col justify-center gap-2">
+            {filteredTransactions.map((transaction, index) => (
+              <TransactionItem
+                key={index}
+                title={transaction.title}
+                price={transaction.price}
+                category={transaction.category}
+                date={transaction.date}
+                type={transaction.type}
               />
-
-              {/* Search Button */}
-              <button className="flex items-center ring-yellow-500 ring-2 rounded-sm p-3 text-sm text-yellow-500 font-bold transition-transform active:scale-95">
-                <MagnifyingGlassIcon className="h-5 w-5 text-yellow-500 mx-2" />
-                <div className="hidden sm:flex">Buscar</div>
-              </button>
-            </div>
-
-            {/* Histórico de Transações */}
-            <div className="flex flex-col justify-center gap-2">
-              {/* Transações */}
-              {transactions.map((transaction, index) => (
-                <TransactionItem
-                  key={index}
-                  title={transaction.title}
-                  price={transaction.price}
-                  category={transaction.category}
-                  date={transaction.date}
-                  type={transaction.type}
-                />
-              ))}
-            </div>
+            ))}
           </div>
         </div>
 
